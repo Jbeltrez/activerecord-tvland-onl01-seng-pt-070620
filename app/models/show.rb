25ad954e4ab756bb61cf9f771e2444e7a84736df
Jbeltrez <beltrezjohn@gmail.com>
2020-10-02 13:25:05 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters 
+    has_many :actors, through: :characters
+    belongs_to :network 
+    def actors_list 
+        # show.actors_list = an array of all actors in that show 
+        # actors exist in a show by the characters they play 
+        self.actors.collect {|actor| actor.full_name}
+    end 
+
 end
---
+++ db/migrate/006_add_catchphrase_to_character.rb
@@ -0,0 +1,4 @@
+class AddCatchphraseToCharacter < ActiveRecord::Migration[5.1]
+    add_column :characters, :catchphrase, :string
+end 
+
